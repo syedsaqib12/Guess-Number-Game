02a18f1a02be3d27ff2ec10e51cfc6012572c4be
syedsaqib12 <49809570+syedsaqib12@users.noreply.github.com>
2019-07-25 16:42:00 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,55 @@ function guessTheNumber() {
         alert("CONGRATULATIONS" + wrongNumberguess + "Times Wrong");
     }
 }
+
+let randomeNumberMedium = parseInt(Math.random() * 200);
+let wrongNumberguessMedium = 0;
+
+
+
+
+function mediumTheNumber() {
+    let userInput = parseInt(document.querySelector("input").value);
+    wrongNumberguessMedium++;
+    if (userInput < randomeNumberMedium) {
+        alert("Too Small");
+
+    }
+    else if (userInput > randomeNumberMedium) {
+        alert("Too Big");
+
+    } else if (userInput == randomeNumberMedium) {
+        alert("CONGRATULATIONS" + wrongNumberguessMedium + "Times Wrong");
+    }
+}
+
+let randomeNumberHard = parseInt(Math.random() * 200);
+let wrongNumberguessHard = 0;
+
+
+
+
+function HardTheNumber() {
+    let userInput = parseInt(document.querySelector("input").value);
+    wrongNumberguessHard++;
+    if (userInput < randomeNumberHard) {
+        alert("Too Small");
+
+    }
+    else if (userInput > randomeNumberHard) {
+        alert("Too Big");
+
+    } else if (userInput == randomeNumberHard) {
+        alert("CONGRATULATIONS" + wrongNumberguessHard + "Times Wrong");
+    }
+}
+
+function goToEasyMode() {
+    window.location = "index.html"
+}
+function goToMediumMode() {
+    window.location = "indexMedium.html"
+}
+function goToHardMode() {
+    window.location = "indexHard.html"
+}
